c62e4478f699c3df5e25ba489d4371af31900cbf
Gabriel Carniel <gpcarniel@outlook.com>
2023-09-04 19:54:03 -0300

    corrigindo estrutura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
+import { cn } from '@/utils/tw-merge'
+import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { cn } from './utils/tw-merge'
-import Header from './components/layout/header'
-import Footer from './components/layout/footer'
+import Header from '@/components/layout/header'
+import Footer from '@/components/layout/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
